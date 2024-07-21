a71af581e3e884a15954263f676f047a107208e6
trivernis <trivernis@protonmail.com>
2024-07-21 18:19:13 +0200

    Add corpus tests

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -6,6 +6,10 @@ mod types;
 use crate::parse;
 
 #[test]
-fn it_parses() {
-    parse("hello world").unwrap();
+fn it_parse_types() {
+    let result = parse(include_str!("../../corpus/types.sk"));
+
+    if let Err(e) = result {
+        panic!("{e}")
+    }
 }
